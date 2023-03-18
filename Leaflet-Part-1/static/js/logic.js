1bdde0be79ba4ec504a0d0ec8d9275013df6a08d
Ivan Villa <itanalytics21@gmail.com>
2023-03-18 15:21:41 -0700

    Leaflet Update

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,7 +1,8 @@
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(data){
     console.log(data);
     console.log(data.features[0].geometry.coordinates[0])
-    var quakeMarkers = [];
+    
+    var colorList = ["lightgreen","yellow","orange","red","darkred"]
 
     for (var i =0; i < data.features.length; i++){
         latitude = data.features[i].geometry.coordinates[0];
@@ -10,11 +11,26 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
         magnitude = data.features[i].properties.mag;
         place = data.features[i].properties.place
 
-        L.circle([latitude,longitude], {
-            color: "red",
-            fillColor: "red",
-            fillOpacity: .5 + depth/1200,
-            radius: magnitude * 25000
+        let cNum = ""
+
+        if (depth <= 10) {
+          cNum = 0
+        } else if (depth <= 50) {
+          cNum = 1
+        } else if (depth <= 100) {
+          cNum = 2
+        } else if (depth <= 250) {
+          cNum = 3
+        } else {
+          cNum = 4
+        }
+
+        L.circleMarker([latitude,longitude], {
+            color:"black",
+            weight: .5,
+            fillColor: colorList[cNum],
+            fillOpacity: 1,
+            radius: magnitude * 2
           }).addTo(myMap).bindPopup(
             `Location: ${place}<br />
              Magnitude: ${magnitude}<br />
@@ -25,12 +41,13 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
 
 var myMap = L.map("map", {
     center: [40.4168, -3.7038],
-    zoom: 2
+    zoom: 3
   });
 
 var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(myMap);
+
 
 
 
